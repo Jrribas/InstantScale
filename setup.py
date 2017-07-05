c71fe57fcb50a7e8b71e4be6ea089c0dabb777a3
Warfront <Warfront@users.noreply.github.com>
2017-07-05 10:31:26 +0100

    Include shutil setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ includes      = []
 include_files = [r"C:\Users\Farinha\Anaconda3\DLLs\tcl86t.dll", \
                  r"C:\Users\Farinha\Anaconda3\DLLs\tk86t.dll",
                  r"C:\Users\Farinha\Documents\GitHub\InstantScale\TesseractPath.txt"]
-packages = ["numpy"]
+packages = ["numpy","shutil"]
 
 setup(name='InstantScale',
     version = '0.1',
     description='Parse stuff',
     options = {"build_exe": {"includes": includes, "include_files": include_files, "packages": packages}},
-    executables = [Executable("main.py",targetName="InstantScale.exe", shortcutName="InstantScale", shortcutDir="DesktopFolder", icon="icon.ico")]) 
+    executables = [Executable("main.py",targetName="InstantScale.exe", shortcutName="InstantScale", shortcutDir="DesktopFolder", icon="icon.ico")])
