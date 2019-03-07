1d7d0bb48ae1b7925fab54fc1ede784a7a2c0962
rFarinha <rFarinha@users.noreply.github.com>
2019-03-07 13:16:08 +0000

    test-tkinter
    UI done

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 09:45:11 2019
+
+@author: Farinha
+"""
+x = 0
+while x <5:
+    if x == 0:
+        print("ola")
+        y = 1
+        x = x + 1
+    else:
+        print(y)
+        x = x + 1
